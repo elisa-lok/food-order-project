3cee946030eef3605b1fffcb2a240e53d5214676
Elisa <elisalok413@gmail.com>
2024-06-28 10:11:04 +1200

    use userProgressContext

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,20 +4,31 @@ import CartContext from "../store/CartContext.jsx";
 import CartItem from "./CartItem.jsx";
 import { currencyFormatter } from "../util/formatter.js";
 import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const userProgressCxt = useContext(UserProgressContext);
+
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
-  function handleClose() {}
+  function handleClose() {
+    userProgressCxt.hideCart();
+  }
 
-  function handleCheckout() {}
+  function handleCheckout() {
+    userProgressCxt.showCheckout();
+  }
 
   return (
-    <Modal>
+    <Modal
+      className="cart"
+      open={userProgressCxt.progress === "cart"}
+      onClose={userProgressCxt.progress === "cart" ? handleClose : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
