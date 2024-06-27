2ad26a3bc849c079deae51f17dcb1ef6aeaa7b2d
Elisa <elisalok413@gmail.com>
2024-06-27 21:25:21 +1200

    render cart and cartItem

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,43 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext.jsx";
+import CartItem from "./CartItem.jsx";
+import { currencyFormatter } from "../util/formatter.js";
+import Button from "./UI/Button.jsx";
+
 export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {}
+
+  function handleCheckout() {}
+
   return (
-    <h1>this is cart page.</h1>
+    <Modal>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textButton onClick={handleClose}>
+          Close
+        </Button>
+        <Button onClick={handleCheckout}>Go to Checkout</Button>
+      </p>
+    </Modal>
   );
 }
-
